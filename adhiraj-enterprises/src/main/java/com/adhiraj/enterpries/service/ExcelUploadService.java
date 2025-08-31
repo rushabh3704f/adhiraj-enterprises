91307dcf13bad6793897d1923e43345a9a12a76f
vishwakarmasanjay368 <vishwakarmasanjay368@gmail.com>
2025-08-31 20:47:37 +0530

    commit

--- adhiraj-enterprises/src/main/java/com/adhiraj/enterpries/service/ExcelUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.adhiraj.enterpries.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ExcelUploadService {
-
-	public void saveExcelData(MultipartFile file);
-
-}
